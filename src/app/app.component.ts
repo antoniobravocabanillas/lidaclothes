7c139b46031c43249c386d5b2876f770a92029dc
antoniobravocabanillas <antoniobravo@gmx.com>
2024-06-09 16:08:56 -0300

    Agregando correciones a los datos de contacto

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { headerLida } from './components/header/header.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Lida Clothes';
+}
